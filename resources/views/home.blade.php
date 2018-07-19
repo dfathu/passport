abeda7599beddabbc49322f5ed1b0e851d1d8c6e
dfathu <dfathu@gmail.com>
2018-07-19 13:33:00 +0700

    node js

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,8 +16,8 @@
 
                     <div id="app">
                       <passport-clients></passport-clients>
-  <passport-authorized-clients></passport-authorized-clients>
-  <passport-personal-access-tokens></passport-personal-access-tokens>
+                      <passport-authorized-clients></passport-authorized-clients>
+                      <passport-personal-access-tokens></passport-personal-access-tokens>
                     </div>
 
                 </div>
